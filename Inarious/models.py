ae48256f170316f7e5f8cde1e686d67f2e3b19df
jeremy <engjat94@gmail.com>
2023-07-01 18:48:27 -0400

    created a poetry env and renamed the app folder to Inarious

---
+++ Inarious/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True, autoIncrement=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    middle_name = Column(String)
+    gender = Column(String)
