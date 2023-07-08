f81d6d451402ced018d1c7bb16d483d976d4b735
jeremy <engjat94@gmail.com>
2023-07-08 10:06:39 -0400

    fixing fasut main to work for basic example

--- Inarious/faust_main.py
+++ Inarious/faust_main.py
@@ -5,15 +5,16 @@ class UserMethod(faust.Record):
     method: str
 
 
-app = faust.App('inarious-app', broker='kafka://localhost:19092')
-users_kafka_topic = app.topic("Users")
+app = faust.App(
+    "inarious-app", broker="kafka://localhost:19092", value_serializer="raw"
+)
+users_kafka_topic = app.topic("Users", key_type=str, value_type=str)
 
 
 @app.agent(users_kafka_topic)
 async def process(user_method_stream):
-    print(user_method_stream)
-    async for method in user_method_stream:
-        print(f"method: {method}")
+    async for key, method in user_method_stream.items():
+        print(f"key: {key} method: {method}")
 
 
 if __name__ == "__main__":
